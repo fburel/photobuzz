458ed364cef311f6d498f5a5086632429088c7bf
Florian BUREL <florian.burel@mac.com>
2015-01-08 10:41:15 +0100

    Exercice

--- photobuzz/CarousselViewController.m
+++ photobuzz/CarousselViewController.m
@@ -13,29 +13,21 @@
 
 @property (weak, nonatomic) IBOutlet FBImageCaroussel *caroussel;
 
+//TODO: Ajouter une property NSArray * pictures
+
 @end
 
 @implementation CarousselViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    //TODO: Récuperer la liste des Pictures via la repository (location au choix)
+    
+    // TODO: Se declarer en datasource du caroussel
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+//TODO: Implementer le FBImageCarousselDataSource
 
 @end
